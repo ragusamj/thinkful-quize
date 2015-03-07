f8ec5af84925cda10d8cbf6023ce7d002e80006e
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 15:13:01 -0800

    set up html with emmet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Thinkful Quize App</title>
+  <script src="/assets/js/data.js"></script>
+  <script src="/assets/js/app.js"></script>
+  <link rel="stylesheet" href="/assets/css/style.css">
+<body>
+  
+</body>
+</html>
