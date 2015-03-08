c920fb4ff692e7d4de2281040e1510b0dd584983
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 16:37:35 -0800

    changed script to point to grunt minified version

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Thinkful Quize App</title>
-  <script src="/assets/js/data.js"></script>
-  <script src="/assets/js/app.js"></script>
+  <script src="/js/app.min.js"></script>
   <link rel="stylesheet" href="/assets/css/style.css">
 <body>
   
