4e6a0711a2fe4169f6cc7d365195b979210c8c86
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 16:36:51 -0800

    adding schema

---
+++ assets/js/data.js
@@ -0,0 +1,74 @@
+{
+  "form": "Creative Brief",
+  "requester": {
+    "name": {
+      "first": "String",
+      "last": "String"
+    },
+    "email": "String",
+    "department": "String"
+  },
+  "questions": [
+    {
+      "question": "What is the project name?",
+      "answer": "String",
+      "required": true,
+      "tag": {
+        "name": "input",
+        "type": "text"
+      }
+    },
+    {
+      "question": "What type of project?",
+      "answer": "String",
+      "required": true,
+      "tag": {
+        "name": "select",
+        "options": [
+          {
+            "name": "Fix / Patch / Issue",
+            "value": "Fix / Patch / Issue"
+          },
+          {
+            "name": "Marketing Campaign",
+            "value": "Marketing Campaign"
+          },
+          {
+            "name": "Web Site Enhancment",
+            "value": "Web Site Enhancment"
+          },
+          {
+            "name": "Event Assets",
+            "value": "Event Assets"
+          }
+        ]
+      }
+    },
+    {
+      "question": "What type of project?",
+      "answer": "String",
+      "required": true,
+      "tag": {
+        "name": "select",
+        "options": [
+          {
+            "name": "Fix / Patch / Issue",
+            "value": "Fix / Patch / Issue"
+          },
+          {
+            "name": "Marketing Campaign",
+            "value": "Marketing Campaign"
+          },
+          {
+            "name": "Web Site Enhancment",
+            "value": "Web Site Enhancment"
+          },
+          {
+            "name": "Event Assets",
+            "value": "Event Assets"
+          }
+        ]
+      }
+    }
+  ]
+}
