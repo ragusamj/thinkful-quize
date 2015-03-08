a0c81d913c3db2df6305074ef2b82dde60dc69d5
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 16:39:31 -0800

    add meta tag

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Thinkful Quize App</title>
   <script src="/js/app.min.js"></script>
   <link rel="stylesheet" href="/assets/css/style.css">
