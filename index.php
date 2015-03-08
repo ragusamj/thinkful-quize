9cacf273105ffffdd4d241043295c73489ad41a2
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-07 16:48:48 -0800

    adding basic html structure

--- index.php
+++ index.php
@@ -8,7 +8,18 @@
   <link rel="stylesheet" href="/assets/css/style.css">
 <body>
   <div class="container-fluid">
-  ...
-	</div>
+  	<section id="breadcrumb" class="col-xs-12">
+  		<ul>
+  		
+  		
+  		</ul>
+  	</section>
+	  <section id="questions" class="col-xs-12">	
+	  	<label id="question" class="required">Question</label>
+	  	<div id="answer" class=""><input type="text"   /></div>
+	  </section>
+	  <section id="actions" class="actions col-xs-12">	
+		  <button></button>
+	  </section>
 </body>
 </html>
