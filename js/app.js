1c13a090f6964b16bb92fcc334239ca3502285c2
Stephen Gehring <sgehringdesign@gmail.com>
2015-03-08 18:42:10 -0700

    adding json to variable so it builds
      may move into function

--- js/app.js
+++ js/app.js
@@ -11509,3 +11509,103 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+;var quize = {
+  "form": "Front End Quize",
+  "questions": [
+    {
+      "question": "What does HTML stand for?",
+      "answer": "String",
+      "required": true,
+      "answer": "HyperText Markup Language",
+      "tag": {
+        "name": "input",
+        "type": "text"
+      }
+    },
+    {
+      "question": "What does CSS stand for?",
+      "answer": "String",
+      "required": true,
+      "answer": "Cascading Style Sheets",
+      "tag": {
+        "name": "input",
+        "type": "text"
+      }
+    },
+    {
+      "question": "What is Emmet?",
+      "answer": "String",
+      "required": true,
+      "answer": "A plug in for fast code short cuts",
+      "tag": {
+        "name": "input",
+        "type": "radio",
+        "options": [
+          {
+            "name": "A city in Nebraska",
+            "value": "A city in Nebraska"
+          },
+          {
+            "name": "A web application for web developers",
+            "value": "A web application for web developers"
+          },
+          {
+            "name": "A plug in for fast code short cuts",
+            "value": "A plug in for fast code short cuts"
+          },
+          {
+            "name": "The heraldic ant",
+            "value": "The heraldic ant"
+          }
+        ]
+      }
+    },
+    {
+      "question": "Enter the id selector in CSS?",
+      "answer": "String",
+      "required": true,
+      "answer": "#",
+      "tag": {
+        "name": "input",
+        "type": "text"
+      }
+    },
+    {
+      "question": "Enter the class selector in CSS?",
+      "answer": "String",
+      "required": true,
+      "answer": ".",
+      "tag": {
+        "name": "input",
+        "type": "text"
+      }
+    },
+    {
+      "question": "What is the correct example of a attribute selector in CSS?",
+      "answer": "String",
+      "required": true,
+      "tag": {
+        "name": "input",
+        "type": "radio",
+        "options": [
+          {
+            "name": "input>type.text",
+            "value": "input>type.text"
+          },
+          {
+            "name": "input#type>text",
+            "value": "input#type>text"
+          },
+          {
+            "name": "input{type=text}",
+            "value": "input{type=text}"
+          },
+          {
+            "name": "input[type=text]",
+            "value": "input[type=text]"
+          }
+        ]
+      }
+    }
+  ]
+}
